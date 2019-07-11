30f1932d09fc48aab29b94c4c1245633eb3169fa
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-07-11 21:20:42 +0300

    +

--- ThreadsMacOS01/main.swift
+++ ThreadsMacOS01/main.swift
@@ -1,12 +1,36 @@
-//
-//  main.swift
-//  ThreadsMacOS01
-//
-//  Created by Артем on 7/11/19.
-//  Copyright © 2019 Артем. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+var thread = pthread_t(bitPattern: 0) //нулевой поток
+var atr = pthread_attr_t() //атрибут для потока
 
+pthread_attr_init(&atr) //указатель на атрибут в области памяти
+
+pthread_create(&thread, &atr, { (pointer) -> UnsafeMutableRawPointer? in
+    print("testOK")
+    
+    for i in 0...10000000000 {
+        if i % 100_000 == 0 {
+            print(i)
+        }
+    }
+    print("???")
+    
+    return nil
+}, nil) //создание потока
+
+//readLine()
+
+var nstread = Thread {
+    
+    for i in 0...1000000000 {
+        if i % 100_000 == 0 {
+            print("ku-ku")
+        }
+    }
+}
+
+nstread.start()
+nstread.cancel()
+nstread.isMainThread
+
+Thread.setThreadPriority(<#T##p: Double##Double#>)
